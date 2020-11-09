710c9a57abee50a51c1b51b1b5eafd2fcb9a7e87
MikeZorin <mykhailo.zorin@gmail.com>
2020-11-09 14:11:55 -0600

    Add project files.

---
+++ ASPproject/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPproject.Controllers
+{
+    public class StudentController : Controller
+    {
+        public string Index(int id, string name)
+        {
+            return "Hello from Student Controller  id = " + id
+                // + " name " + Request.Query["name"]
+                + " name =  " + name
+                ;
+        }
+
+        public string GetDetails()
+        {
+            return "GetDetails() has been invoked!";
+        }
+    }
+}
