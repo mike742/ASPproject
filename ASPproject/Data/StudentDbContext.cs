0b0f90685bf2dbdea977a158054380744004bd5f
MikeZorin <mykhailo.zorin@gmail.com>
2020-11-16 13:05:18 -0600

    added Student Validation

---
+++ ASPproject/Data/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ASPproject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPproject.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"server=localhost;user=root;password=;database=aspdb;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ASPproject/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPproject.Models
+{
+    public class Student
+    {
+        public int Id { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$", 
+            ErrorMessage ="Invalid Email Format")]
+        public string Email { set; get; }
+    }
+}
